40e4e01bc4245a5e9e92850ae4308f175b0303fd
stuartemery96 <stuemery96@gmail.com>
2024-01-03 14:45:01 -0800

    functionality for show and index

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,11 +2,17 @@ const Skill = require('../models/skill');
 
 
 module.exports = {
-  index
-}
+  index,
+  show
+};
 
 function index(req, res) {
   res.render('skills/index', {
     skills: Skill.getAll()
   });
+}
+
+function show(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/show', { skill });
 }
--- models/skill.js
+++ models/skill.js
@@ -1,11 +1,17 @@
 const skills = [
-  { id: 1234, skill: 'Javascript' },
-  { id: 1235, skill: 'CSS' },
-  { id: 1236, skill: 'HTML' }
+  { id: 1234, skill: 'Javascript', desc: 'Code that makes stuff happen' },
+  { id: 1235, skill: 'CSS', desc: 'Code that styles the page' },
+  { id: 1236, skill: 'HTML', desc: 'Code that shows up on the GUI' }
 ];
 
 module.exports = {
-  getAll
+  getAll,
+  getOne
+}
+
+function getOne(id) {
+  id = parseInt(id);
+  return skills.find((skill) => skill.id === id);
 }
 
 function getAll() {
